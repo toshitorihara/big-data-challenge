0a35ae66164e251e454c52e26f3ecc33da6a33b5
phenomenon1223 <phenomenon1223@gmail.com>
2021-10-23 23:05:54 -0700

    final submission

--- level-1/schema.sql
+++ level-1/schema.sql
@@ -1,28 +1,21 @@
-CREATE TABLE review_id_table (
-  review_id TEXT PRIMARY KEY NOT NULL,
-  customer_id INTEGER,
-  product_id TEXT,
-  product_parent INTEGER,
-  review_date DATE -- this should be in the formate yyyy-mm-dd
-);
+DROP TABLE IF EXISTS video_game_vine_reviews;
+DROP TABLE IF EXISTS sofware_vine_reviews;
 
--- This table will contain only unique values
-CREATE TABLE products (
-  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-  product_title TEXT
-);
-
--- Customer table for first data set
-CREATE TABLE customers (
-  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
-  customer_count INT
-);
-
--- vine table
-CREATE TABLE vine_table (
+CREATE TABLE video_game_vine_reviews (
   review_id TEXT PRIMARY KEY,
   star_rating INTEGER,
   helpful_votes INTEGER,
   total_votes INTEGER,
   vine TEXT
 );
+
+CREATE TABLE sofware_vine_reviews (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT
+);
+
+SELECT * FROM video_game_vine_reviews;
+SELECT * FROM sofware_vine_reviews;
